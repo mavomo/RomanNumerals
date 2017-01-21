382d4ba7df6377a31f2148add100b504ce32ed25
mavomo <michelle.avomo@gmail.com>
2017-01-21 16:17:04 +0100

    Dictionnary complete

--- src/main/java/io/training/katas/RomanNumeral.java
+++ src/main/java/io/training/katas/RomanNumeral.java
@@ -1,13 +1,30 @@
 package io.training.katas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Michelle on 21/01/2017.
  */
 public class RomanNumeral {
+
     public static String converFromArabic(int number) {
-        if (number == 5){
-            return "V";
-        }
-        return "I";
+
+        Map<Integer, String> romanNumerals = buildRomanNumeralCharts();
+
+        return romanNumerals.get(number);
+    }
+
+    private static Map<Integer, String> buildRomanNumeralCharts() {
+        Map<Integer, String> romanNumeralsCharts = new HashMap<>();
+        romanNumeralsCharts.put(1, "I");
+        romanNumeralsCharts.put(5, "V");
+        romanNumeralsCharts.put(10, "X");
+        romanNumeralsCharts.put(50, "L");
+        romanNumeralsCharts.put(100, "L");
+        romanNumeralsCharts.put(500, "D");
+        romanNumeralsCharts.put(1000, "M");
+
+        return romanNumeralsCharts;
     }
 }
--- src/test/java/io/training/katas/RomanNumeralsTest.java
+++ src/test/java/io/training/katas/RomanNumeralsTest.java
@@ -11,13 +11,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RomanNumeralsTest {
 
     @Test
-    public void should_return_I_given_1(){
+    public void should_return_I_given_number_1(){
         assertThat(RomanNumeral.converFromArabic(1)).isEqualTo("I");
     }
 
     @Test
-    public void should_return_V_given_5(){
+    public void should_return_V_given_number_5(){
         assertThat(RomanNumeral.converFromArabic(5)).isEqualTo("V");
+    }
+
+    @Test
+    public void should_return_X_given_number_10(){
+        assertThat(RomanNumeral.converFromArabic(10)).isEqualTo("X");
+    }
+
+    @Test
+    public void should_return_L_given_number_50(){
+        assertThat(RomanNumeral.converFromArabic(50)).isEqualTo("L");
+    }
+
+    @Test
+    public void should_return_C_given_number_100(){
+        assertThat(RomanNumeral.converFromArabic(100)).isEqualTo("C");
+    }
+
+    @Test
+    public void should_return_D_given_number_500(){
+        assertThat(RomanNumeral.converFromArabic(500)).isEqualTo("D");
+    }
+
+
+    @Test
+    public void should_return_M_given_number_1000(){
+        assertThat(RomanNumeral.converFromArabic(1000)).isEqualTo("M");
     }
 
 }
