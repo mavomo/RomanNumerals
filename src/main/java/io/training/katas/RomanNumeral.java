34b5ab281e697c4e3806ace8e9cd8e1532fa8551
mavomo <michelle.avomo@gmail.com>
2017-01-21 16:07:42 +0100

    second step

--- src/main/java/io/training/katas/RomanNumeral.java
+++ src/main/java/io/training/katas/RomanNumeral.java
@@ -4,7 +4,10 @@ package io.training.katas;
  * Created by Michelle on 21/01/2017.
  */
 public class RomanNumeral {
-    public static String converFromArabic(String s) {
+    public static String converFromArabic(int number) {
+        if (number == 5){
+            return "V";
+        }
         return "I";
     }
 }
--- src/test/java/io/training/katas/RomanNumeralsTest.java
+++ src/test/java/io/training/katas/RomanNumeralsTest.java
@@ -3,6 +3,8 @@ package io.training.katas;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by Michelle on 21/01/2017.
  */
@@ -10,10 +12,12 @@ public class RomanNumeralsTest {
 
     @Test
     public void should_return_I_given_1(){
-        String arabicLetter = RomanNumeral.converFromArabic("1");
-        Assertions.assertThat(arabicLetter).isEqualTo("I");
-
+        assertThat(RomanNumeral.converFromArabic(1)).isEqualTo("I");
     }
 
+    @Test
+    public void should_return_V_given_5(){
+        assertThat(RomanNumeral.converFromArabic(5)).isEqualTo("V");
+    }
 
 }
