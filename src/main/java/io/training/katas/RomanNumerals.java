49487875a3f38bad1118ba9d7ccc747e056ed469
mavomo <michelle.avomo@gmail.com>
2017-02-08 22:33:29 +0100

    Refactor all > 100

--- src/main/java/io/training/katas/RomanNumerals.java
+++ src/main/java/io/training/katas/RomanNumerals.java
@@ -7,55 +7,58 @@ public class RomanNumerals {
 
 
     public static String convertToArabic(int number) {
+
         StringBuilder value = new StringBuilder();
 
-        if (number ==100){
+        int numberToDecrement = 100;
+
+        while (number >= 100){
             value.append("C");
-            number -=100;
-        }
-        if (number == 50){
-            value.append("L");
-            number -= 50;
+            number -=numberToDecrement;
         }
 
-        if (number > 50){
-            value.append("L");
-            number -= 50;
-        }
-
-        while (number > 10){
-            value.append("X");
-            number -=10;
-        }
-
-        if (number == 10){
-            value.append("X");
-        }
-
-        if (number > 5 && number<10) {
-            value.append("V");
-            number -= 5;
-        }
-
-/*        if (number == 20){
-            value.append("X");
+      /*  if (number == numberToDecrement){
+            value.append("C");
+            number -=numberToDecrement;
         }*/
-        if (number == 5){
+
+        numberToDecrement = 50;
+        if (number == numberToDecrement){
+            value.append("L");
+            number -= numberToDecrement;
+        }
+
+        if (number > numberToDecrement){
+            value.append("L");
+            number -= numberToDecrement;
+        }
+
+        numberToDecrement = 10;
+        while (number > numberToDecrement){
+            value.append("X");
+            number -=numberToDecrement;
+        }
+
+        if (number == numberToDecrement){
+            value.append("X");
+        }
+
+        numberToDecrement = 5;
+        if (number > numberToDecrement && number<10) {
+            value.append("V");
+            number -= numberToDecrement;
+        }
+
+        if (number == numberToDecrement){
             value.append("V");
         }
 
-    if(number < 5) {
+    if(number < numberToDecrement) {
         for (int i = 1; i <= number; i++) {
             value.append("I");
         }
     }
-        /*if (number == 2){
-            return "II";
-        }
-        if(number == 3){
-            return "III";
-        }
-        return "I";*/
+
         return  value.toString();
     }
 }
--- src/test/java/io/training/katas/RomanNumeralTestCase.java
+++ src/test/java/io/training/katas/RomanNumeralTestCase.java
@@ -3,6 +3,8 @@ package io.training.katas;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by Michelle on 08/02/2017.
  */
@@ -10,65 +12,70 @@ public class RomanNumeralTestCase {
 
     @Test
     public void should_return_I_given_1(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(1)).isEqualTo("I");
+        assertThat(RomanNumerals.convertToArabic(1)).isEqualTo("I");
     }
 
 
     @Test
     public void should_return_II_given_2(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(2)).isEqualTo("II");
+        assertThat(RomanNumerals.convertToArabic(2)).isEqualTo("II");
     }
 
 
     @Test
     public void should_return_III_given_3(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(3)).isEqualTo("III");
+        assertThat(RomanNumerals.convertToArabic(3)).isEqualTo("III");
     }
 
     @Test
     public void should_return_V_given_5(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(5)).isEqualTo("V");
+        assertThat(RomanNumerals.convertToArabic(5)).isEqualTo("V");
     }
 
     @Test
     public void should_return_VI_given_6(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(6)).isEqualTo("VI");
+        assertThat(RomanNumerals.convertToArabic(6)).isEqualTo("VI");
     }
 
     @Test
     public void should_return_X_given_10(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(10)).isEqualTo("X");
+        assertThat(RomanNumerals.convertToArabic(10)).isEqualTo("X");
     }
 
     @Test
     public void should_return_XI_given_11(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(11)).isEqualTo("XI");
+        assertThat(RomanNumerals.convertToArabic(11)).isEqualTo("XI");
     }
 
 
     @Test
     public void should_return_XX_given_20(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(20)).isEqualTo("XX");
+        assertThat(RomanNumerals.convertToArabic(20)).isEqualTo("XX");
     }
 
     @Test
     public void should_return_XXX_given_30(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(30)).isEqualTo("XXX");
+        assertThat(RomanNumerals.convertToArabic(30)).isEqualTo("XXX");
     }
 
 
     @Test
     public void should_return_L_given_50(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(50)).isEqualTo("L");
+        assertThat(RomanNumerals.convertToArabic(50)).isEqualTo("L");
     }
 
     @Test
     public void should_return_LXXVII_given_77(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(77)).isEqualTo("LXXVII");
+        assertThat(RomanNumerals.convertToArabic(77)).isEqualTo("LXXVII");
     }
 
     @Test
     public void should_return_C_given_100(){
-        Assertions.assertThat(RomanNumerals.convertToArabic(100)).isEqualTo("C");
+        assertThat(RomanNumerals.convertToArabic(100)).isEqualTo("C");
+    }
+
+    @Test
+    public void should_return_CC_given_200(){
+        assertThat(RomanNumerals.convertToArabic(200)).isEqualTo("CC");
     }
 }
