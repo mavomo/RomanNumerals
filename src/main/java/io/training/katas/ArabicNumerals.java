6266531b2ec75357cfc90749b74e858c9ff88110
mavomo <michelle.avomo@gmail.com>
2017-02-09 06:44:59 +0100

    Refactoring using reversed ordered Map

--- src/main/java/io/training/katas/ArabicNumerals.java
+++ src/main/java/io/training/katas/ArabicNumerals.java
@@ -1,6 +1,9 @@
 package io.training.katas;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 /**
@@ -11,14 +14,30 @@ public class ArabicNumerals {
 
     public static Integer convertToRomanNumeral(String romanNumeral) {
 
+
+        Map<Character, Integer> arabicNumeralsCharts = new TreeMap<>(Collections.reverseOrder());
+
+        arabicNumeralsCharts.put('X', 10);
+        arabicNumeralsCharts.put('V', 5);
+        arabicNumeralsCharts.put('I', 1);
+
         List<Character> romanCharacters = romanNumeral.chars().mapToObj(r -> (char)r).collect(Collectors.toList());
 
         int value = 0;
+        for (int index =0; index < romanCharacters.size(); index++){
+            Character currentChar = romanCharacters.get(index);
+            value += arabicNumeralsCharts.get(currentChar);
+            romanCharacters.remove(index);
+        }
+
+        for (int i=0; i< romanCharacters.size(); i++){
+            value ++;
+        }
         if (romanNumeral == "XVIII"){
             return  18;
         }
 
-        if (romanNumeral == "X"){
+       /* if (romanNumeral == "X"){
             value =10;
             romanCharacters.remove(0);
         }
@@ -29,11 +48,9 @@ public class ArabicNumerals {
        /* if (romanNumeral == "VI"){
             value = 5;
             romanCharacters.remove(0);
-        }*/
-
-        for (int i=0; i< romanCharacters.size(); i++){
-            value ++;
         }
+
+
        /* if (romanNumeral == "II"){
             return 2;
         }
