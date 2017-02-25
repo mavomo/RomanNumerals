5c78e39fb13a08ff7aad472befa081f2be5bd51c
mavomo <michelle.avomo@gmail.com>
2017-02-25 14:58:36 +0100

    Refactoring X usage

--- src/main/java/io/training/katas/new_version/ArabicNumeralsConverter.java
+++ src/main/java/io/training/katas/new_version/ArabicNumeralsConverter.java
@@ -1,5 +1,12 @@
 package io.training.katas.new_version;
 
+import com.sun.deploy.util.ArrayUtil;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Michelle on 25/02/2017.
  */
@@ -7,6 +14,18 @@ public class ArabicNumeralsConverter {
 
     public static int convertFromRoman(String romanNumeral) {
         int arabicNumber = 0;
+        Character[] romanNumeralsAsChars = romanNumeral.chars().mapToObj(c -> (char)c).toArray(Character[]::new);
+
+        List<Character> romanNumerals = new ArrayList<>(Arrays.asList(romanNumeralsAsChars));
+
+         for (int i=0; i < romanNumerals.size();){
+             if (romanNumeral.startsWith("X")){
+                 arabicNumber += 10;
+                 romanNumerals.remove(i);
+             }
+    }
+
+/*
         if (romanNumeral.equals("X")){
             return 10;
         }
@@ -16,8 +35,11 @@ public class ArabicNumeralsConverter {
         if (romanNumeral.equals("XXX")){
             return 30;
         }
-        for (int i= 0; i < romanNumeral.toCharArray().length; i++){
-            arabicNumber++;
+*/
+        if (!romanNumerals.isEmpty()) {
+            for (int i = 0; i < romanNumerals.size(); i++) {
+                arabicNumber++;
+            }
         }
 
         return arabicNumber;
