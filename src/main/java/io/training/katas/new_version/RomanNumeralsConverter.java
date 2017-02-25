01d8cefe7ab85df0dc4b848270c0eafda6fd8311
mavomo <michelle.avomo@gmail.com>
2017-02-25 13:45:23 +0100

    Refactoring the addition of X and I

--- src/main/java/io/training/katas/new_version/RomanNumeralsConverter.java
+++ src/main/java/io/training/katas/new_version/RomanNumeralsConverter.java
@@ -6,29 +6,16 @@ package io.training.katas.new_version;
 public class RomanNumeralsConverter {
 
     public static String convertFromArabic(int arabicNumeral) {
-/*
-        if (arabicNumeral == 10){
-            return "X";
-        }
-        if (arabicNumeral == 20){
-            return "XX";
-        }
-        if (arabicNumeral == 30){
-            return "XXX";
-        }*/
+
      StringBuilder romanNumeralValue = new StringBuilder();
      for (;10 <= arabicNumeral;){
             romanNumeralValue.append("X");
             arabicNumeral -= 10;
      }
-
-     if (arabicNumeral > 0) {
-         romanNumeralValue = new StringBuilder("I");
-
-         for (int i = 1; i < arabicNumeral; i++) {
+     for (int i = 1; i <= arabicNumeral; i++) {
              romanNumeralValue.append("I");
-         }
      }
+
      return romanNumeralValue.toString();
     }
 }
