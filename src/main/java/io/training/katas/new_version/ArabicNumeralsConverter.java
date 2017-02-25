eea0cea8eeaa3b95013fc8ff7af5dd36ae414991
mavomo <michelle.avomo@gmail.com>
2017-02-25 14:23:43 +0100

    Refactor

--- src/main/java/io/training/katas/new_version/ArabicNumeralsConverter.java
+++ src/main/java/io/training/katas/new_version/ArabicNumeralsConverter.java
@@ -6,6 +6,12 @@ package io.training.katas.new_version;
 public class ArabicNumeralsConverter {
 
     public static int convertFromRoman(String romanNumeral) {
+        int arabicNumber = 0;
+
+        for (int i= 0; i < romanNumeral.toCharArray().length; i ++){
+            arabicNumber++;
+        }
+/*
         if (romanNumeral.equals("I")){
             return 1;
         }
@@ -16,7 +22,7 @@ public class ArabicNumeralsConverter {
         if (romanNumeral.equals("III")){
             return 3;
         }
-
-        return 0;
+*/
+        return arabicNumber;
     }
 }
