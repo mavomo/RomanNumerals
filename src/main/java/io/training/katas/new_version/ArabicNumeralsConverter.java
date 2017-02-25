d637dffcf20832a2f0952999fc6f1f0f9208512d
mavomo <michelle.avomo@gmail.com>
2017-02-25 15:32:05 +0100

    Iterating through a reverse ordered map to find value

--- src/main/java/io/training/katas/new_version/ArabicNumeralsConverter.java
+++ src/main/java/io/training/katas/new_version/ArabicNumeralsConverter.java
@@ -1,11 +1,8 @@
 package io.training.katas.new_version;
 
-import com.sun.deploy.util.ArrayUtil;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by Michelle on 25/02/2017.
@@ -14,17 +11,21 @@ public class ArabicNumeralsConverter {
 
     public static int convertFromRoman(String romanNumeral) {
         int arabicNumber = 0;
-        while (romanNumeral.length() > 0 ){
-             if (romanNumeral.startsWith("X")){
-                 arabicNumber+= 10;
-                 romanNumeral = romanNumeral.substring(1);
-             }
-             if(romanNumeral.startsWith("I")){
-                 arabicNumber+=1;
-                 romanNumeral = romanNumeral.substring(1);
-             }
-        }
+        Map<String, Integer> arabicNumeralsChart = new TreeMap<>(Collections.reverseOrder());
+        arabicNumeralsChart.put("X", 10);
+        arabicNumeralsChart.put("I", 1);
 
+        for (String romanNumeralKey : arabicNumeralsChart.keySet()){
+            int arabicNumeralValue = arabicNumeralsChart.get(romanNumeralKey);
+            while (romanNumeral.startsWith(romanNumeralKey)){
+                    arabicNumber+= arabicNumeralValue;
+                    romanNumeral = romanNumeral.substring(1);
+                }
+            /*    if(romanNumeral.startsWith("I")){
+                    arabicNumber+=1;
+                    romanNumeral = romanNumeral.substring(1);
+                }*/
+            }
         return arabicNumber;
     }
 }
